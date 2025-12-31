f064ba0df4f686e290d956db305a0f3c569dc185
rohit15c <63947504+rohit15c@users.noreply.github.com>
2025-12-31 10:45:07 +0000

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ const CONFIG = {
   ],
 
   // Put your 3 images in assets with these exact names:
-  photos: ["assets/photo1.jpg", "assets/photo2.jpg", "assets/photo3.jpg"],
+  photos: ["assets/1.jpg", "assets/2.jpg", "assets/3.jpg"],
 
   // If your assets fail for any reason, these show instead:
   fallbackPhotos: [
